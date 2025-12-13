9b96a5ab412cc3eab80a5d45061982b42d4d72f8
XRikzy <rosendoricardo8@outlook.com>
2025-12-13 12:47:12 -0500

    Delete Crud types (not used)

--- src/types/crud.types.ts
+++
@@ -1,6 +0,0 @@
-import { CreateContactDto } from '@/sites/dto/create-contact.dto';
-import { CreateSiteDto } from '@/sites/dto/create-site.dto';
-import { UpdateSiteDto } from '@/sites/dto/update-site.dto';
-
-export type CrudCreateDTO = CreateSiteDto | CreateContactDto;
-export type CrudUpdateDTO = UpdateSiteDto;
